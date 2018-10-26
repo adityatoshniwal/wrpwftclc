65033d25cd799ceba4312e4045a75f4f156969a3
Aditya Toshniwal <aditya.toshniwal@enterprisedb.com>
2018-10-26 09:36:57 +0530

    Moving towards Relational structure than using JSON

---
+++ fieldcodes.sql
@@ -0,0 +1,59 @@
+insert into field_codes(field_code, field_name) values ("weftMetre", "Metre" );
+insert into field_codes(field_code, field_name) values ("warpPanna", "Panna" );
+insert into field_codes(field_code, field_name) values ("outPerctg", "Percentage" );
+insert into field_codes(field_code, field_name) values ("warpWtWstg", "Warp Wt. with Wastage" );
+insert into field_codes(field_code, field_name) values ("peek", "Peek" );
+insert into field_codes(field_code, field_name) values ("rateOutRs", "Rate Out Rs." );
+insert into field_codes(field_code, field_name) values ("weftWtWstg", "Weft Wt. with Wastage" );
+insert into field_codes(field_code, field_name) values ("weavingChrg", "Weaving Charges" );
+insert into field_codes(field_code, field_name) values ("cramp", "Cramp" );
+insert into field_codes(field_code, field_name) values ("weftReedSpace", "Reed Space" );
+insert into field_codes(field_code, field_name) values ("id", "ID" );
+insert into field_codes(field_code, field_name) values ("totalCost", "Total Cost" );
+insert into field_codes(field_code, field_name) values ("totalWtWstg", "Total Wt. with Wastage" );
+insert into field_codes(field_code, field_name) values ("weftWt", "Weight" );
+insert into field_codes(field_code, field_name) values ("localPerctg", "Percentage %" );
+insert into field_codes(field_code, field_name) values ("warpMetre", "Metre" );
+insert into field_codes(field_code, field_name) values ("title", "Name" );
+insert into field_codes(field_code, field_name) values ("reed", "Reed" );
+insert into field_codes(field_code, field_name) values ("demandRateLocal","Demanded Local Out" );
+insert into field_codes(field_code, field_name) values ("lassa", "Lassa (yards)" );
+insert into field_codes(field_code, field_name) values ("demandRateOut", "Demanded Rate Out" );
+insert into field_codes(field_code, field_name) values ("warpReedSpace", "Reed Space" );
+insert into field_codes(field_code, field_name) values ("rateOutPer", "Percentage %" );
+insert into field_codes(field_code, field_name) values ("totalWt", "Total Weight" );
+insert into field_codes(field_code, field_name) values ("weftPanna", "Panna" );
+insert into field_codes(field_code, field_name) values ("totalEnds", "Total Ends" );
+insert into field_codes(field_code, field_name) values ("rateLocalRs", "Rate Local Rs." );
+insert into field_codes(field_code, field_name) values ("rateLocalPer", "Rate Local Per" );
+insert into field_codes(field_code, field_name) values ("jobRate", "Job Rate" );
+insert into field_codes(field_code, field_name) values ("warpWt", "Warp Weight" );
+insert into field_codes(field_code, field_name) values ("warpGCount","Count" );
+insert into field_codes(field_code, field_name) values ("warpGPerct","Perct." );
+insert into field_codes(field_code, field_name) values ("warpGWstg","Wastage" );
+insert into field_codes(field_code, field_name) values ("warpGRate","Rate" );
+insert into field_codes(field_code, field_name) values ("warpGSizRate","Sizing Rate" );
+insert into field_codes(field_code, field_name) values ("warpGWt","Weight" );
+insert into field_codes(field_code, field_name) values ("warpGCost","Warp Cost" );
+insert into field_codes(field_code, field_name) values ("warpSizCost","Warp Sizing Cost" );
+insert into field_codes(field_code, field_name) values ("weftGCount","Count" );
+insert into field_codes(field_code, field_name) values ("weftGPerct","Perct." );
+insert into field_codes(field_code, field_name) values ("weftGWstg","Wastage" );
+insert into field_codes(field_code, field_name) values ("weftGRate","Rate" );
+insert into field_codes(field_code, field_name) values ("weftGWt","Weight" );
+insert into field_codes(field_code, field_name) values ("weftGCost","Weft Cost" );
+insert into field_codes(field_code, field_name) values ("warppackGKgBag","Kg/Bag" );
+insert into field_codes(field_code, field_name) values ("warppackGConeBag","No. of cone/bag" );
+insert into field_codes(field_code, field_name) values ("warppackGPart","Part" );
+insert into field_codes(field_code, field_name) values ("warppackGBeam","No Of Beam" );
+insert into field_codes(field_code, field_name) values ("warppackGKgCone","Kg/Cone" );
+insert into field_codes(field_code, field_name) values ("warppackGMeger","Total Meger" );
+insert into field_codes(field_code, field_name) values ("warppackGTara","Tara" );
+insert into field_codes(field_code, field_name) values ("warppackGBags","No Of Bags" );
+insert into field_codes(field_code, field_name) values ("warppackGCut","Total Cut" );
+insert into field_codes(field_code, field_name) values ("warppackGCutBeam","Cut On Beam" );
+
+
+
+
+
